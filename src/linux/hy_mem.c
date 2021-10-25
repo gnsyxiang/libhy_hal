195b6b2516693ee986f839ebbc0eda729d9dfbd5
zhenquan.qiu <gnsyxiang@163.com>
2021-10-25 19:54:24 +0800

    feat: add hy_mem.h

---
+++ src/linux/hy_mem.c
@@ -0,0 +1,53 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_mem.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    25/10 2021 19:26
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        25/10 2021      create the file
+ * 
+ *     last modified: 25/10 2021 19:26
+ */
+#include <stdio.h>
+
+#include "hy_mem.h"
+
+#include "hy_utils/hy_log.h"
+
+#define ALONE_DEBUG 1
+
+void *HyMemMalloc(size_t size)
+{
+    LOGE("HyMemMalloc failed \n");
+
+    return NULL;
+}
+
+void HyMemFree(void **pptr)
+{
+    LOGE("HyMemFree failed \n");
+}
+
+void *HyMemCalloc(size_t nmemb, size_t size)
+{
+    LOGE("HyMemCalloc failed \n");
+
+    return NULL;
+}
+
+void *HyMemRealloc(void *ptr, size_t nmemb, size_t size)
+{
+    LOGE("HyMemRealloc failed \n");
+
+    return NULL;
+}
+
