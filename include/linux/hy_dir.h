94c026c1491f4e85d3eb65c04cd114bdc3ed3160
zhenquan.qiu <gnsyxiang@163.com>
2021-10-30 09:05:42 +0800

    feat: add hy_dir.h

---
+++ include/linux/hy_dir.h
@@ -0,0 +1,81 @@
+/**
+ *
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_dir.h
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    30/10 2021 08:59
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        30/10 2021      create the file
+ * 
+ *     last modified: 30/10 2021 08:59
+ */
+#ifndef __LIBHY_HAL_INCLUDE_HY_DIR_H_
+#define __LIBHY_HAL_INCLUDE_HY_DIR_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+/**
+ * @brief 类型
+ */
+typedef enum {
+    HY_DIR_TYPE_FILE,       ///< 文件
+    HY_DIR_TYPE_DIR,        ///< 目录
+
+    HY_DIR_TYPE_MAX,
+} HyDirType_t;
+
+/**
+ * @brief 读取到文件的回调函数
+ *
+ * @param path 文件所在的路径
+ * @param name 文件名
+ * @param type 文件的类型，详见HyDirType_t
+ * @param args 上层传递的参数
+ */
+typedef void (*HyDirReadCb_t)(const char *path, const char *name,
+        uint8_t type, void *args);
+
+/**
+ * @brief 读取目录下的所有文件
+ *
+ * @param path 需要读取的路径名
+ * @param filter 过滤后缀，NULL为不过滤
+ * @param read_cb 读取到文件的回调函数，详见HyDirReadCb_t
+ * @param args 上层传递的参数
+ *
+ * @return 成功返回0，失败返回-1
+ */
+int32_t HyDirRead(const char *path, const char *filter,
+        HyDirReadCb_t read_cb, void *args);
+
+/**
+ * @brief 递归读取目录下的所有文件
+ *
+ * @param path 需要读取的路径名
+ * @param filter 过滤后缀，NULL为不过滤
+ * @param read_cb 读取到文件的回调函数，详见HyDirReadCb_t
+ * @param args 上层传递的参数
+ *
+ * @return 成功返回0，失败返回-1
+ */
+int32_t HyDirReadRecurse(const char *path, const char *filter,
+        HyDirReadCb_t read_cb, void *args);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ src/linux/hy_dir.c
@@ -0,0 +1,131 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_dir.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    30/10 2021 09:00
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        30/10 2021      create the file
+ * 
+ *     last modified: 30/10 2021 09:00
+ */
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "hy_dir.h"
+
+#include "hy_type.h"
+#include "hy_string.h"
+#include "hy_log.h"
+
+#define ALONE_DEBUG 1
+
+static void _filter_file(const char *path, const char *name,
+        uint8_t type, void *args,
+        HyDirReadCb_t read_cb, const char *filter)
+{
+    char buf[HY_STRING_BUF_MAX_LEN_64] = {0};
+    size_t len;
+
+    if (!read_cb) {
+        return;
+    }
+
+    if (filter) {
+        buf[0] = '.';
+        len = strlen(filter);
+        HyStrCopyRight(name, buf + 1, HY_STRING_BUF_MAX_LEN_64, '.');
+        if (len > HY_STRING_BUF_MAX_LEN_64) {
+            LOGE("the suffix is too long \n");
+            return;
+        } else {
+            if (0 == strncmp(buf, filter, strlen(filter))) {
+                read_cb(path, name, type, args);
+            }
+        }
+    } else {
+        read_cb(path, name, type, args);
+    }
+}
+
+static void _handle_sub_dir(const char *path, char *name, const char *filter,
+        HyDirReadCb_t handle_cb, void *args)
+{
+    size_t len = strlen(path) + strlen(name) + 1 + 1; // 1 for space('\0'), 1 for '/'
+    char *sub_path = malloc(len);
+    if (!sub_path) {
+        LOGE("malloc failed \n");
+        return ;
+    }
+    memset(sub_path, '\0', len);
+
+    snprintf(sub_path, len, "%s/%s", path, name);
+    HyDirReadRecurse(sub_path, filter, handle_cb, args);
+
+    free(sub_path);
+}
+
+static int32_t _dir_read(int32_t type, const char *path, const char *filter,
+        HyDirReadCb_t read_cb, void *args)
+{
+    DIR *dir;
+    struct dirent *ptr;
+
+    dir = opendir(path);
+    if (!dir) {
+        LOGE("opendir failed \n");
+        return -1;
+    }
+
+    while ((ptr = readdir(dir)) != NULL) {
+        if (strcmp(ptr->d_name, ".") == 0 || strcmp(ptr->d_name, "..") == 0) {
+            continue;
+        }
+
+        switch (ptr->d_type) {
+            case DT_REG:
+                _filter_file(path, ptr->d_name, HY_DIR_TYPE_FILE, args,
+                        read_cb, filter);
+                break;
+
+            case DT_DIR:
+                if (type) {
+                    _handle_sub_dir(path, ptr->d_name, filter, read_cb, args);
+                } else {
+                    _filter_file(path, ptr->d_name, HY_DIR_TYPE_FILE, args,
+                            read_cb, filter);
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    closedir(dir);
+
+    return 0;
+}
+
+int32_t HyDirRead(const char *path, const char *filter,
+        HyDirReadCb_t read_cb, void *args)
+{
+    return _dir_read(0, path, filter, read_cb, args);
+}
+
+int32_t HyDirReadRecurse(const char *path, const char *filter,
+        HyDirReadCb_t read_cb, void *args)
+{
+    return _dir_read(1, path, filter, read_cb, args);
+}
+
