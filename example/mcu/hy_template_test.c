9fe507f8e1e425ccbbb9644dc11d4a84bcb1b798
zhenquan.qiu <gnsyxiang@163.com>
2021-11-08 20:10:08 +0800

    feat: add hy_template_test.c for mcu

---
+++ example/mcu/hy_template_test.c
@@ -0,0 +1,104 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_template_test.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    08/11 2021 19:30
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        08/11 2021      create the file
+ * 
+ *     last modified: 08/11 2021 19:30
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "hy_type.h"
+#include "hy_mem.h"
+#include "hy_string.h"
+#include "hy_module.h"
+#include "hy_uart.h"
+#include "hy_hal_utils.h"
+#include "hy_log.h"
+
+#define ALONE_DEBUG 1
+
+typedef struct {
+    void *debug_uart_handle;
+    void *log_handle;
+
+    hy_s32_t exit_flag;
+} _main_context_t;
+
+static void _module_destroy(_main_context_t **context_pp)
+{
+    _main_context_t *context = *context_pp;
+
+    // note: 增加或删除要同步到module_create_t中
+    module_destroy_t module[] = {
+        {"log",         &context->log_handle,           HyLogDestroy},
+        {"debug_uart",  &context->debug_uart_handle,    HyUartDestroy},
+    };
+
+    RUN_DESTROY(module);
+
+    HY_MEM_FREE_PP(context_pp);
+}
+
+static _main_context_t *_module_create(void)
+{
+    _main_context_t *context = HY_MEM_MALLOC_RET_VAL(_main_context_t *, sizeof(*context), NULL);
+
+    HyUartConfig_t debug_uart_config;
+    debug_uart_config.config_save.num       = DEBUG_UART_NUM;
+    debug_uart_config.config_save.read_cb   = NULL;
+    debug_uart_config.config_save.args      = context;
+    debug_uart_config.rate                  = HY_UART_RATE_115200;
+    debug_uart_config.flow_control          = HY_UART_FLOW_CONTROL_NONE;
+    debug_uart_config.bits                  = HY_UART_BITS_8;
+    debug_uart_config.parity                = HY_UART_PARITY_N;
+    debug_uart_config.stop                  = HY_UART_STOP_1;
+
+    HyLogConfig_t log_config;
+    log_config.save_config.buf_len      = 512;
+    log_config.save_config.level        = HY_LOG_LEVEL_TRACE;
+    log_config.save_config.color_output = HY_TYPE_FLAG_ENABLE;
+
+    // note: 增加或删除要同步到module_destroy_t中
+    module_create_t module[] = {
+        {"debug_uart",  &context->debug_uart_handle,    &debug_uart_config,     (create_t)HyUartCreate,     HyUartDestroy},
+        {"log",         &context->log_handle,           &log_config,            (create_t)HyLogCreate,      HyLogDestroy},
+    };
+
+    RUN_CREATE(module);
+
+    return context;
+}
+
+int main(int argc, char *argv[])
+{
+    _main_context_t *context = _module_create();
+    if (!context) {
+        LOGE("_module_create faild \n");
+        return -1;
+    }
+
+    LOGE("version: %s, data: %s, time: %s \n", "0.1.0", __DATE__, __TIME__);
+
+    for (;;) {
+    }
+
+    _module_destroy(&context);
+
+    return 0;
+}
+
