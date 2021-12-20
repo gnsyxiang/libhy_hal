71d4cfb5b25a33ebeecd3166ca516515f22a0a33
zhenquan.qiu <gnsyxiang@163.com>
2021-12-20 19:47:31 +0800

    feat: add hy_time files

---
+++ include/linux/hy_time.h
@@ -0,0 +1,69 @@
+/**
+ *
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_time.h
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    20/12 2021 14:31
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        20/12 2021      create the file
+ * 
+ *     last modified: 20/12 2021 14:31
+ */
+#ifndef __LIBHY_HAL_INCLUDE_HY_TIME_H_
+#define __LIBHY_HAL_INCLUDE_HY_TIME_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * @brief 获取utc时间
+ *
+ * @return 秒数
+ */
+time_t HyTimeGetUTC(void);
+
+/**
+ * @brief 获取当前时间
+ *
+ * @param tm 当前时间
+ */
+void HyTimeGetLocalTime(struct tm *tm);
+
+/**
+ * @brief 获取并格式化当前时间
+ *
+ * @param buf 存储数组
+ * @param len 数组的长度
+ *
+ * @note 2021-12-20_19-00-00，时分秒不用分号的原因是在fat32文件系统中无法识别
+ */
+void HyTimeFormatLocalTime(char *buf, size_t len);
+
+/**
+ * @brief 把格式化时间转成UTC时间
+ *
+ * @param data_time 被格式化的时间
+ *
+ * @return UTC时间
+ *
+ * @note 2021-12-20_19-00-00，时分秒不用分号的原因是在fat32文件系统中无法识别
+ */
+time_t HyTimeFormatTime2UTC(const char *data_time);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/linux/hy_time.c
@@ -0,0 +1,71 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_time.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    20/12 2021 19:16
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        20/12 2021      create the file
+ * 
+ *     last modified: 20/12 2021 19:16
+ */
+#include <stdio.h>
+
+#include "hy_time.h"
+
+#include "hy_log.h"
+#include "hy_type.h"
+#include "hy_mem.h"
+#include "hy_string.h"
+#include "hy_assert.h"
+
+#define ALONE_DEBUG 1
+
+time_t HyTimeGetUTC(void)
+{
+    return time(NULL);
+}
+
+void HyTimeGetLocalTime(struct tm *tm)
+{
+    time_t t = time(NULL);
+    localtime_r(&t, tm);    // localtime_r可重入，localtime不可重入
+}
+
+void HyTimeFormatLocalTime(char *buf, size_t len)
+{
+    struct tm tm;
+
+    HY_MEMSET(buf, len);
+
+    HyTimeGetLocalTime(&tm);
+
+    snprintf(buf, len, "%04d-%02d-%02d_%02d-%02d-%02d",
+            tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
+            tm.tm_hour, tm.tm_min, tm.tm_sec);
+
+    // 跟上面的效果一样，strftime可以生成许多的格式
+    // strftime(buf, len, "%Y-%m-%d_%H-%M-%S", &tm);
+}
+
+time_t HyTimeFormatTime2UTC(const char *data_time)
+{
+    struct tm tm;
+
+    sscanf(data_time, "%04d-%02d-%02d_%02d-%02d-%02d",
+            &tm.tm_year, &tm.tm_mon, &tm.tm_mday,
+            &tm.tm_hour, &tm.tm_min, &tm.tm_sec);
+
+    tm.tm_year  -= 1900;
+    tm.tm_mon   -= 1;
+
+    return mktime(&tm);
+}
