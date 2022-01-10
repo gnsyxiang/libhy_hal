b897217415cf4dae8bf54228f9b000dcf6c32cf1
zhenquan.qiu <gnsyxiang@163.com>
2022-01-10 16:51:44 +0800

    feat: add func for hy_file

--- include/linux/hy_file.h
+++ include/linux/hy_file.h
@@ -26,9 +26,50 @@ extern "C" {
 
 #include <stdio.h>
 
+/**
+ * @brief 从文件中读取数据
+ *
+ * @param fd 文件fd
+ * @param buf 存放数据的地址
+ * @param len 读取的长度
+ *
+ * @return 返回读到的字节数，失败放回-1，被中断返回0
+ */
 ssize_t HyFileRead(int fd, void *buf, size_t len);
 
-ssize_t HyFileWrite(int fd, const void *buf, size_t len);
+/**
+ * @brief 从文件中读取数据
+ *
+ * @param fd 文件fd
+ * @param buf 存放数据的地址
+ * @param len 读取的长度
+ *
+ * @return 返回读到指定的字节数，失败放回-1或0
+ */
+ssize_t HyFileReadN(int fd, void *buf, size_t len);
+
+/**
+ * @brief 从文件中读取数据
+ *
+ * @param fd 文件fd
+ * @param buf 存放数据的地址
+ * @param cnt 读取数据的长度
+ * @param ms 超时时间
+ *
+ * @return 返回读到指定的字节数，失败放回-1或0
+ */
+ssize_t HyFileReadNTimeout(int fd, void *buf, size_t cnt, size_t ms);
+
+/**
+ * @brief 向文件写入数据
+ *
+ * @param fd 文件fd
+ * @param buf 存放数据的地址
+ * @param len 写入的数据长度
+ *
+ * @return 成功返回len，失败返回-1
+ */
+ssize_t HyFileWriteN(int fd, const void *buf, size_t len);
 
 #ifdef __cplusplus
 }
--- src/linux/hy_file.c
+++ src/linux/hy_file.c
@@ -21,38 +21,46 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdint.h>
+/* According to POSIX.1-2001, POSIX.1-2008 */
+#include <sys/select.h>
+
+/* According to earlier standards */
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "hy_log.h"
+#include "hy_type.h"
 
 #include "hy_file.h"
 
-#include "hy_log.h"
-
-ssize_t HyFileWrite(int fd, const void *buf, size_t len)
+ssize_t HyFileRead(int fd, void *buf, size_t len)
 {
     ssize_t ret;
-    size_t nleft;
-    const void *ptr;
 
-    ptr   = buf;
-    nleft = len;
-
-    while (nleft > 0) {
-        ret = write(fd, ptr, nleft);
-        if (ret <= 0) {
-            if (ret < 0 && errno == EINTR) {
+    while (1) {
+        ret = read(fd, buf, len);
+        if (ret < 0) {
+            if (EINTR == errno || EAGAIN == errno || EWOULDBLOCK == errno) {
                 ret = 0;
+                continue;
             } else {
-                return -1;
+                ret = -1;
+                break;
             }
+        } else if (ret == 0) {
+            LOGE("fd: %d, server/client closes \n", fd);
+            ret = -1;
+            break;
+        } else {
+            break;
         }
-
-        nleft -= ret;
-        ptr   += ret;
     }
 
-    return len;
+    return ret;
 }
 
-ssize_t HyFileRead(int fd, void *buf, size_t len)
+ssize_t HyFileReadN(int fd, void *buf, size_t len)
 {
     ssize_t ret;
     size_t nleft;
@@ -79,4 +87,63 @@ ssize_t HyFileRead(int fd, void *buf, size_t len)
     }
 
     return offset;
+}
+
+ssize_t HyFileReadNTimeout(int fd, void *buf, size_t cnt, size_t ms)
+{
+    hy_s32_t len = 0;
+    fd_set rfds;
+    struct timeval time;
+
+    FD_ZERO(&rfds);
+    FD_SET(fd, &rfds);
+
+	time.tv_sec = ms / 1000;
+	time.tv_usec = (ms % 1000) * 1000;
+
+    size_t ret = select(fd+1, &rfds, NULL, NULL, &time);
+    switch (ret) {
+        case -1:
+            LOGE("select error");
+            cnt = -1;
+            break;
+        case 0:
+            LOGE("select timeout");
+            cnt = -1;
+            break;
+        default:
+            if ((len = HyFileReadN(fd, buf, cnt)) == -1) {
+                LOGE("read error");
+                cnt = -1;
+            }
+            break;
+    }
+
+    return cnt;
+}
+
+ssize_t HyFileWriteN(int fd, const void *buf, size_t len)
+{
+    ssize_t ret;
+    size_t nleft;
+    const void *ptr;
+
+    ptr   = buf;
+    nleft = len;
+
+    while (nleft > 0) {
+        ret = write(fd, ptr, nleft);
+        if (ret <= 0) {
+            if (ret < 0 && errno == EINTR) {
+                ret = 0;
+            } else {
+                return -1;
+            }
+        }
+
+        nleft -= ret;
+        ptr   += ret;
+    }
+
+    return len;
 }
