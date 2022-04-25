41e39508e77b465d76b472c65253c56ddb9b1c7b
zhenquan.qiu <gnsyxiang@163.com>
2022-04-25 18:48:14 +0800

    feat: add socket_ipc.h

---
+++ include/linux/log/socket_ipc.h
@@ -0,0 +1,32 @@
+/**
+ *
+ * Release under GPLv-3.0.
+ * 
+ * @file    socket_ipc.h
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    25/04 2022 18:41
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        25/04 2022      create the file
+ * 
+ *     last modified: 25/04 2022 18:41
+ */
+#ifndef __LIBHY_HAL_INCLUDE_SOCKET_IPC_H_
+#define __LIBHY_HAL_INCLUDE_SOCKET_IPC_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
